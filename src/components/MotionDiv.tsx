ae6ad8b1a79b2839d7eda201153869f4742ad3bd
Amirreza Mahmoudi <amirrezamh03@gmail.com>
2024-03-13 19:02:46 +0330

    done

---
+++ src/components/MotionDiv.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { FADE } from "./FrameMotion";
+
+interface Props {
+  children: React.ReactNode;
+}
+function MotionDiv({ children }: Props) {
+  return (
+    <motion.div
+      key={Math.random().toString()}
+      variants={FADE}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      {children}
+    </motion.div>
+  );
+}
+export default MotionDiv;
